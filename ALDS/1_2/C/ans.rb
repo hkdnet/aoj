b68cdca5dce396f6241220c851797bd7bcf139c1
hkdnet <hkdnet@users.noreply.github.com>
2015-04-05 03:38:48 +0900

    add Card class

--- ALDS/1_2/C/ans.rb
+++ ALDS/1_2/C/ans.rb
@@ -12,6 +12,33 @@ module Input
 end
 
 #
+# data-class
+#
+class Card
+  include Comparable
+
+  attr_reader :suit, :rank
+
+  def initialize(c)
+    @c = c
+    @suit = c[0]
+    @rank = c[1].to_i
+  end
+
+  def <=>(other)
+    @rank <=> other.rank
+  end
+
+  def to_s
+    @c
+  end
+
+  def equal?(other)
+    @c.to_s == other.to_s
+  end
+end
+
+#
 # main logic which has only one public method Solver#solve
 #
 class Solver
@@ -19,7 +46,7 @@ class Solver
 
   def initialize
     @n = geti
-    @arr = gets.chomp.split
+    @arr = gets.chomp.split.map { |c| Card.new(c) }
     @arr_1 = @arr.dup
     @arr_2 = @arr.dup
     @arr = nil
@@ -27,7 +54,7 @@ class Solver
 
   def solve
     bubble_sort
-    puts 'Stable'
+    puts 'Stable' # bubble sort is always stable
     selection_sort
     puts match?(@arr_1, @arr_2) ? 'Stable' : 'Not stable'
   end
@@ -35,7 +62,7 @@ class Solver
   def bubble_sort
     (0...@n).each do |i|
       (@n - 1).downto(i + 1) do |j|
-        swap(@arr_1, j - 1, j) if rank(@arr_1[j]) < rank(@arr_1[j - 1])
+        swap(@arr_1, j - 1, j) if @arr_1[j] < @arr_1[j - 1]
       end
     end
 
@@ -53,14 +80,14 @@ class Solver
   def selection_sort
     (0...@n).each do |i|
       mini = i
-      (i...@n).each { |j| mini = j if rank(@arr_2[j]) < rank(@arr_2[mini]) }
+      (i...@n).each { |j| mini = j if @arr_2[j] < @arr_2[mini] }
       swap(@arr_2, mini, i)
     end
     puts @arr_2.join(' ')
   end
 
   def match?(a1, a2)
-    a1.zip(a2).all? { |e1, e2| e1 == e2 }
+    a1.zip(a2).all? { |e1, e2| e1.equal?(e2) }
   end
 end
 
